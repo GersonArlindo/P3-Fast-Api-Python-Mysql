d10745b834818d09636d08901243ff390afcdb9e
GersonArlindo <gerson50039@gmail.com>
2023-05-29 21:43:56 -0600

    Agregando Schema

--- router/router.py
+++ router/router.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
+from schema.user_schema import UserSchema
 
 user = APIRouter()
 
 @user.get("/")
 def root():
     return {"message": " Hi, I am FastApi with router" }
+
+@user.post("/api/user")
+def create_user(data_user: UserSchema):
+    print(data_user)
