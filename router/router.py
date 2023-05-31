ff4a3dbe6c57bade6a46ac83b58c5b6999a4b5e2
GersonArlindo <gerson50039@gmail.com>
2023-05-30 19:59:48 -0600

    Creando conexiona db

--- router/router.py
+++ router/router.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
 from schema.user_schema import UserSchema
+from config.db import conn
+from model.users import users
 
 user = APIRouter()
 
@@ -9,4 +11,11 @@ def root():
 
 @user.post("/api/user")
 def create_user(data_user: UserSchema):
-    print(data_user)
+    try:
+        new_user = data_user.dict()
+        print(new_user)
+        conn.execute(users.insert().values(new_user))
+        conn.commit()
+        return "Success"
+    except Exception as e:
+        return f"Error: {str(e)}"
